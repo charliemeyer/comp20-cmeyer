a677e9de1db433bb005ae72c62ddb298b65bbdeb
Charles Meyer <charles.meyer@tufts.edu>
2015-02-25 12:54:00 -0500

    drew slice of spritesheet onto canvas

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,10 @@
-// Replace this with your code...
+function init() {
+    console.log("hello world");
+    var canvas = document.getElementById('game_canvas');
+    var ctx = canvas.getContext("2d");
+    var img = new Image();   // Create new img element
+    img.addEventListener("load", function() {
+        ctx.drawImage(img, 400, 0, 200, 100, 0, 0, 200, 100);
+    }, false);
+    img.src = 'pacman10-hp-sprite.png'; // Set source path
+}
