794d0fc4825480f58affb3eb104ece340977e1ca
Charles Meyer <charles.meyer@tufts.edu>
2015-03-05 14:18:57 -0500

    added some error handling

--- messages/lab.js
+++ messages/lab.js
@@ -6,13 +6,15 @@ function parse(){
 }
 
 function parseData(data){
-    if(request.readyState == 4){
+    if(request.readyState == 4 && response.status == 200){
         responseObject = JSON.parse(request.responseText);
         messages = document.getElementById("messages");
         responseObject.forEach(function(messageObject){
             messages.innerHTML += '<p>' + messageObject['content'] + ' ' + '<span>'
                                         + messageObject['username'] + '</span></p>';
         });
+    } else if(request.readyState == 4 && response.status != 200){
+        console.log("request failed");
     } else {
         console.log("Still working");
     }
